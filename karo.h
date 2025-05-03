82e9127645992e10b7aa7adf0180895981ee58ca
karoglueck <kfgluck.ieu2024@student.ie.edu>
2025-05-04 01:13:50 +0200

    update karo.h

--- karo.h
+++ karo.h
@@ -1,53 +1,20 @@
+// karo.h
+#ifndef KARO_H
+#define KARO_H
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "karo.h"
-#include "retrieve.h"
 
-// TO COMPILE: gcc mainoutlineprogram.c retrieve.c karo.c editfunction.c -o mainoutlineprogram --> add the .c functions that you are using as well
+char *title;           // I created pointers for dynamic memory allocation
+char *author;           //This is important for the malloc function below
+char *description;
+char *genres;
+char *url;
+float avg_rating;  //I have chosen float here to enable ratings like 5.7 
+int num_ratings;   // I have chosen int here because the number of ratings in a whole number (e.g. it has 560 ratings)
+char confirm;
 
-void whensave (){
-    int Option2;
-    printf ("Indicate, if you want to edit an existing entry using 1 or create a new entry using 2:");
-    scanf ("%d", &Option2);
+void flush_newline();
+char *read_string(const char *prompt, size_t size);
+void newentry();
 
-    switch (Option2) {
-        case 1:
-        printf ("Editing an existing entry:\n");
-       // editBook();
-        break;
-
-        case 2:
-        printf ("Creating a new entry, please fill out existing information:\n");
-        newentry();
-        break;
-
-        default:
-        printf ("This is an invalid option\n");
-    }
-}
-
-
-
-int main (){
-    
-    int Option1;
-    printf ("Indicate, if you want to save a new entry using 1 or retrieve existing information using 2:");
-    scanf ("%d", &Option1);
-    switch (Option1) {
-        case 1:
-        printf ("Saving a new entry\n");
-        whensave();
-        break;
-
-        case 2:
-        printf ("Retrieving existing information\n"); 
-        //retrieve();
-        break;
-
-        default:
-        printf ("This is an invalid option\n");
-    }
-
-
-}
+#endif
